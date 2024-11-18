e8b829de3046a3afa58aa4d6a30b0c4924399d5b
JdM <julien@rmbi.ch>
2024-11-18 21:31:24 +0100

    fix

--- api/articles.php
+++ api/articles.php
@@ -8,12 +8,8 @@ include_once(dirname(path: __FILE__) . "/utils/database.php");
 
 $db = new Database;
 
-$showInnactives = isset($_GET["showInnactives"]) && $_GET["showInnactives"] == 1;
 
-if ($showInnactives) {
-    $articles = $db-> select("SELECT * FROM internetnews_articles");
-} else {
-    $articles = $db-> select("SELECT * FROM internetnews_articles WHERE active = 1");
-}
+$articles = $db-> select("SELECT * FROM internetnews_articles");
+
 
 echo json_encode($articles);
