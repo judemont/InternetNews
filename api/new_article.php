1f58ee06ce97cfc4d2181fdcbf1bda55bffa30fc
JdM <julien@rmbi.ch>
2024-11-18 21:08:58 +0100

    a

--- api/new_article.php
+++ api/new_article.php
@@ -6,11 +6,10 @@ include_once(dirname(path: __FILE__) . "/utils/database.php");
 
 $db = new Database;
 
-echo $_POST["title"];
 $title = htmlspecialchars($_POST["title"]);
 $description = htmlspecialchars($_POST["description"]);
 $link = htmlspecialchars($_POST["link"]);
 
 
 
-$db -> query("INSERT INTO internetnews_articles (title, description, link, active) VALUES ('%s', '%s', '%s', %s)", [$title, $description, $link, 0]);
+$db -> query("INSERT INTO internetnews_articles (title, description, link, active) VALUES ('?', '?', '?', ?)", [$title, $description, $link, 0]);
